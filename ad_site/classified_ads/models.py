e077c6c677fd670c9faff0bceee49e250f6e4c61
Eric <egh65@cornell.edu>
2023-09-10 14:10:05 -0500

    added str fn to Ad

--- ad_site/classified_ads/models.py
+++ ad_site/classified_ads/models.py
@@ -6,12 +6,14 @@ class Ad(models.Model):
     SELL = "Sell"
     ad_type_choices = [
         (BUY, "Buy"),
-        (SELL, "Sell"),
-        ("None", "None - Placeholder")
+        (SELL, "Sell")
     ]
     ad_text = models.CharField(max_length=200)
     date_posted = models.DateTimeField('date posted')
     ad_type = models.CharField(
         max_length=4, choices=ad_type_choices)
+
+    def __str__(self) -> str:
+        return self.ad_text
 
 # Create your models here.
