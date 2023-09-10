aa61c8267bdc41c454229ad2820ad97c5f219eaf
Eric <egh65@cornell.edu>
2023-09-10 14:08:36 -0500

    made Ad datatable, made admin superuser

--- ad_site/classified_ads/admin.py
+++ ad_site/classified_ads/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Ad
+
+admin.site.register(Ad)
 
 # Register your models here.
--- ad_site/classified_ads/models.py
+++ ad_site/classified_ads/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Ad(models.Model):
+    BUY = "Buy"
+    SELL = "Sell"
+    ad_type_choices = [
+        (BUY, "Buy"),
+        (SELL, "Sell"),
+        ("None", "None - Placeholder")
+    ]
+    ad_text = models.CharField(max_length=200)
+    date_posted = models.DateTimeField('date posted')
+    ad_type = models.CharField(
+        max_length=4, choices=ad_type_choices)
+
 # Create your models here.
