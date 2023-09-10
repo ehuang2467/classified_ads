413f46a0b6d231bedeeda191cac51001519c016e
Eric <egh65@cornell.edu>
2023-09-10 14:50:26 -0500

    ad detail view template made

--- ad_site/classified_ads/views.py
+++ ad_site/classified_ads/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, get_object_or_404
 from .models import Ad
 
 
@@ -8,3 +6,8 @@ def index(request):
     latest_ads_list = Ad.objects.order_by("-date_posted")[:10]
     context = {"latest_ads_list": latest_ads_list}
     return render(request, "classified_ads/index.html", context)
+
+
+def detail(request, ad_id):
+    context = {"parent_ad": get_object_or_404(Ad, pk=ad_id)}
+    return render(request, 'classified_ads/detail.html', context)
