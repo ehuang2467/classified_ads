22df1c1416f1f636cae0740fe413f71497279fb5
Eric <egh65@cornell.edu>
2023-09-10 14:25:05 -0500

    added comment to model

--- ad_site/classified_ads/models.py
+++ ad_site/classified_ads/models.py
@@ -8,13 +8,21 @@ class Ad(models.Model):
         (BUY, "Buy"),
         (SELL, "Sell")
     ]
-    ad_text = models.CharField(max_length=200)
+    ad_text = models.CharField(max_length=1000)
     date_posted = models.DateTimeField('date posted')
     ad_type = models.CharField(
         max_length=4, choices=ad_type_choices)
-    user = models.CharField(max_length=200)
+    user = models.CharField(max_length=30)
 
     def __str__(self) -> str:
         return self.ad_text
 
-# Create your models here.
+
+class Comment(models.Model):
+    parent_ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=1000)
+    date_posted = models.DateTimeField('date posted')
+    user = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.comment_text
