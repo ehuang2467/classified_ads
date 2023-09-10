bed6a3dc45c3a756fbcc626dc334eeace634a3dd
Eric <egh65@cornell.edu>
2023-09-10 16:02:16 -0500

    added back to home button on detail

--- ad_site/classified_ads/urls.py
+++ ad_site/classified_ads/urls.py
@@ -6,5 +6,6 @@ app_name = "classified_ads"
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/comment", views.comment, name="comment"),
     path("<int:pk>/comment", views.comment, name="comment")
 ]
