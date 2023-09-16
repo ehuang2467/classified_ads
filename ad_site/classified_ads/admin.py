66b155bbfb0356c033722efa3be631c2fb06acc2
Eric <ehuang2467@gmail.com>
2023-09-15 23:48:33 -0500

    added comment list and date posted to admin site

--- ad_site/classified_ads/admin.py
+++ ad_site/classified_ads/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Ad, Comment
 
-admin.site.register(Ad)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('text', 'date_posted')
+
+    inlines = [CommentInline]
+
+
+admin.site.register(Ad, AdAdmin)
 admin.site.register(Comment)
+
 
 # Register your models here.
