4757fdcd2fc082930da57c37e398d74698f1601c
Eric <ehuang2467@gmail.com>
2023-09-15 21:39:17 -0500

    ad and comment migrated as subclasses of postable

--- ad_site/classified_ads/models.py
+++ ad_site/classified_ads/models.py
@@ -13,32 +13,19 @@ class Postable(models.Model):
         return self.text
 
 
-class Ad(models.Model):
+class Ad(Postable):
     BUY = "Buy"
     SELL = "Sell"
     ad_type_choices = [
         (BUY, "Buy"),
         (SELL, "Sell")
     ]
-    ad_text = models.CharField(max_length=1000)
-    text = models.CharField(max_length=1000, default="PLACEHOLDER")
-    date_posted = models.DateTimeField()
     ad_type = models.CharField(
         max_length=4, choices=ad_type_choices)
-    user = models.CharField(max_length=30)
-
-    def __str__(self) -> str:
-        return self.ad_text
 
 
-class Comment(models.Model):
+class Comment(Postable):
+    parent_ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+
     class Meta:
         ordering = ['date_posted']
-    parent_ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=1000)
-    text = models.CharField(max_length=1000, default="PLACEHOLDER")
-    date_posted = models.DateTimeField()
-    user = models.CharField(max_length=30)
-
-    def __str__(self) -> str:
-        return self.comment_text
