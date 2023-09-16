02965bd74c5726d0dab3cafce6446cd479294620
Eric <ehuang2467@gmail.com>
2023-09-15 20:59:35 -0500

    made Postable abstract model, updated todo

--- ad_site/classified_ads/models.py
+++ ad_site/classified_ads/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class Postable(models.Model):
+    text = models.CharField(max_length=1000)
+    date_posted = models.DateTimeField()
+    user = models.CharField(max_length=30)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.text
+
+
 class Ad(models.Model):
     BUY = "Buy"
     SELL = "Sell"
