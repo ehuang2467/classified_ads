1e6099df7d093c3bfa5e392237091b60d9fef364
Eric <ehuang2467@gmail.com>
2023-09-21 00:21:37 -0500

    react now works!

--- ad_site/classified_ads/static/classified_ads/index.js
+++ ad_site/classified_ads/static/classified_ads/index.js
@@ -2,7 +2,87 @@
 // var React = require('react');
 // var ReactDOM = require('react-dom/client');
 // import ReactDOM from 'react-dom/client';
-// import App from './App';
+const ads_json = [
+  {
+    "ad_type": "sell",
+    "text": "selling bike", "user": "bob", "date": "1/20/2023"
+  },
+  {
+    "ad_type": "buy",
+    "text": "buy car", "user": "eric", "date": "4/23/2023"
+  }
+]
+
+
+function Filterable_Ads() {
+  const [ad_type, adtype_changer] = React.useState("BuyOrSell")
+  const [search_text, search_text_changer] = React.useState("")
+  return (
+    <div className='filterable_ads'>
+      <AdType_Selector changer={adtype_changer} />
+      <SearchBar changer={search_text_changer} />
+      <AdsList ads={ads_json} ad_type={ad_type} search_text={search_text} />
+    </div>
+  )
+
+}
+
+function AdType_Selector({ changer }) {
+
+  return (
+    <div className='adtype_selector'>
+      <select onChange={(event) => changer(event.target.value)}>
+        <option value="BuyOrSell">Buy or Sell</option>
+        <option value="Buy">Buy</option>
+        <option value="Sell">Sell</option>
+      </select>
+    </div >
+  )
+
+}
+
+function SearchBar({ changer }) {
+  return (
+    <div className='search_bar'>
+      <label>Search:</label>
+      <input type="text" onChange={(event) => changer(event.target.value)}></input>
+    </div>
+  )
+
+}
+
+function AdsList({ ads, ad_type, search_text }) {
+
+  function ad_filter(ad) {
+    return ((ad_type.toUpperCase() == "BuyOrSell".toUpperCase() ||
+      ad_type.toUpperCase() == ad.ad_type.toUpperCase()) &&
+      (search_text == "" || ad.text.includes(search_text)))
+  }
+
+
+  let ads_filtered = ads.filter((ad) => ad_filter(ad))
+  function ad_json_unpacker(ad) {
+    return ad.ad_type + ": " + ad.text + ". Posted by " + ad.user + " on " + ad.date
+  }
+  function ad_json_key(ad) {
+    return ad.user + ad.text
+  }
+  let lis = ads_filtered.map((item) => <li key={ad_json_key(item)}>{ad_json_unpacker(item)}</li>)
+  return (
+    <div className='ads_list'>
+      <ul>{lis}</ul>
+    </div>
+  )
+
+}
+
+
+function App() {
+  return (
+    <Filterable_Ads />
+  );
+}
+// import App from './App.js';
 // var App = require("./App")
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,40 +92,10 @@
 //   </React.StrictMode>
 // );
 
-// ReactDOM.render(<App />, document.querySelector('.app-mountpoint'))
+ReactDOM.render(<App />, document.querySelector('.app-mountpoint'))
 'use strict';
 
-const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return (
-      <button onClick={() => this.setState({ liked: true })}>
-        Like
-      </button>
-    );
-  }
-}
-;
-ReactDOM.render(<LikeButton />, document.querySelector('.app-mountpoint'));
-// window.MyApp = {
-
-//   init: function (opts) {
-//     var mountPoint = document.querySelector(opts.el);
-
-//     React.render(<App />, mountPoint);
-//   }
-
-// };
 // import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
