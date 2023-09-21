4c7bf3ff7dbdf2b3199b0c20cc0b4dbb8160cfbc
Eric <ehuang2467@gmail.com>
2023-09-20 21:17:37 -0500

    added html and body tags to base

---
+++ ad_site/classified_ads/static/classified_ads/test_script.js
@@ -0,0 +1,10 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+// document.getElementById('demo').innerHTML = Date()
+alert("Hello world")
+function myFunction() {
+  alert("Hello from a static file!");
+}
+{/* <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
+  Click me to display Date and Time.
+</button> */}
