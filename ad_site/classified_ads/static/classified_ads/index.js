a0da7e020f20dc1d75110d8acfda942c603dec8f
Eric <ehuang2467@gmail.com>
2023-09-21 00:18:23 -0500

    react now works for dummy like button

--- ad_site/classified_ads/static/classified_ads/index.js
+++ ad_site/classified_ads/static/classified_ads/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+// import React from 'react';
+// var React = require('react');
+// var ReactDOM = require('react-dom/client');
 // import ReactDOM from 'react-dom/client';
 // import App from './App';
+// var App = require("./App")
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -9,15 +12,45 @@ import React from 'react';
 //   </React.StrictMode>
 // );
 
-// function myFunction_index() {
-//   alert("Hello from index.js");
-// }
+// ReactDOM.render(<App />, document.querySelector('.app-mountpoint'))
+'use strict';
 
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
+    );
+  }
+}
+;
+ReactDOM.render(<LikeButton />, document.querySelector('.app-mountpoint'));
+// window.MyApp = {
+
+//   init: function (opts) {
+//     var mountPoint = document.querySelector(opts.el);
+
+//     React.render(<App />, mountPoint);
+//   }
+
+// };
 // import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
 
-document.body.innerHTML = '<div id="app">Hello World!</div>';
+// document.body.innerHTML = '<div id="app">Hello World!</div>';
 
 // Render your React component instead
 // const root = createRoot(document.getElementById('app'));
